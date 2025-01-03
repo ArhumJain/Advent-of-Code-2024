95547af65bf3f4dcd5053d20ec15afcc8836a91b
Arhum Jain <arhumjain10@gmail.com>
2025-01-02 23:56:59 -0800

    day 10

---
+++ Day 10/trailheadP2.cpp
@@ -0,0 +1,62 @@
+#include "../utils/advent.h"
+#include <iostream>
+#include <utility>
+
+using namespace aoc;
+
+constexpr int TEST = 0;
+F f(TEST == 1 ? "test.in" : "main.in");
+
+struct Node {
+  int row, col;
+  int height;
+  int paths = 0;
+  bool explored = false;
+  Node(int height, int row, int col) {
+    this->height = height;
+    this->row = row;
+    this->col = col;
+  }
+};
+
+vec2d<Node*> grid;
+void explore(int row, int col) {
+  Node* node = grid[row][col];
+  node->explored = true;
+  if (node->height == 9) {
+    node->paths++;
+    return;
+  }
+  Node* nextNode;
+  for (auto c: CARDINALS) {
+    if (row + c.first >= 0 && row + c.first < grid.size() && col + c.second >= 0 && col + c.second < grid[0].size()) {
+      nextNode = grid[row + c.first][col + c.second];
+      if (nextNode->height == node->height+1) {
+        if (!nextNode->explored) explore(row + c.first, col + c.second);
+        node->paths += nextNode->paths;
+      }
+    }
+  }
+}
+
+int main() {
+  string line;
+  float row = 0;
+  vec<Node*> trailheads;
+  while (getline(f, line)) {
+    grid.push_back(vec<Node*>());
+    for (int i=0; i<line.size(); i++) {
+      grid[row].push_back(new Node(line[i] - '0', row, i));
+      if (line[i] == '0') {
+        trailheads.push_back(grid[row][i]);
+      }
+    }
+    row++;
+  }
+  int sum = 0;
+  for (Node* trailhead: trailheads) {
+    explore(trailhead->row, trailhead->col);
+    sum += trailhead->paths;
+  }
+  std::cout << sum << std::endl;
+}
